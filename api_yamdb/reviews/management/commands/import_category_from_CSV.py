65f1d2c7cf7a143f89dfa2a1d470addc7608ca6c
Valentin <ya333@yandex.ru>
2024-09-21 23:02:17 +0300

    Импорт csv через basecommand. Пока не удачно.

---
+++ api_yamdb/reviews/management/commands/import_category_from_CSV.py
@@ -0,0 +1,23 @@
+import argparse, csv
+
+from django.core.management.base import BaseCommand, CommandParser
+
+from reviews.models import Category
+
+
+class Command(BaseCommand):
+    help = 'Import CSV'
+
+    # def add_arguments(self, parser):
+    #     parser.add_argument('category', nargs='?', type=str, help='The path to the CSV file')
+
+    def handle(self, *args, **kwargs):
+        #category = kwargs['category']
+        with open('setting.BASE_DIR/static/data/category.csv', 'r', encoding='utf-8') as f:
+            reader = csv.DictReader(f)
+            Category.objects.bulk_create(Category(**row) for row in reader)
+                # category = Category()
+                # category.name = row[0]
+                # category.slug = row[1]
+                # category.save()
+        self.stdout.write(self.style.SUCCESS('Все данные загружены'))
