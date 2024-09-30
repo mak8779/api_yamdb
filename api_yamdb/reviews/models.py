86a76b2c67fa3d7db972952072167696379b69a2
Valentin Bobkov <ya333@yandex.ru>
2024-09-30 13:41:56 +0300

    исправления после ревью

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -4,24 +4,18 @@ from django.db import models
 
 User = get_user_model()
 
-""" Для всех моделей надо добавить class Meta и в нем добавить сортировку.
-Код стайл джанги такой, сначала идет класс Meta, а затем метод str
-    class Meta:
-        ...
-
-    def __str__(self):
-        ...  """
-
 
 class Category(models.Model):
     """Модель категорий."""
 
-    name = models.CharField(max_length=256, verbose_name='Название категории')
+    name = models.CharField(max_length=256, verbose_name='Название')
     slug = models.SlugField(unique=True, max_length=50,
-                            verbose_name='Slug категории')
+                            verbose_name='Slug')
 
     class Meta:
         ordering = ['id']
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
 
     def __str__(self):
         return self.name
@@ -30,12 +24,14 @@ class Category(models.Model):
 class Genre(models.Model):
     """Модель жанров."""
 
-    name = models.CharField(max_length=256, verbose_name='Название жанра')
+    name = models.CharField(max_length=256, verbose_name='Название')
     slug = models.SlugField(unique=True, max_length=50,
-                            verbose_name='Slug жанра')
+                            verbose_name='Slug')
 
     class Meta:
         ordering = ['id']
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
 
     def __str__(self):
         return self.name
@@ -45,25 +41,25 @@ class Title(models.Model):
     """Модель произведений."""
 
     name = models.CharField(max_length=256,
-                            verbose_name='Название произведения')
-    year = models.IntegerField(verbose_name='Год выпуска')
+                            verbose_name='Название')
+    year = models.PositiveSmallIntegerField(verbose_name='Год выпуска',
+                                            db_index=True)
     description = models.TextField(blank=True, null=True,
                                    verbose_name='Описание группы')
     genre = models.ManyToManyField(Genre, through="GenreTitle")
-    """ Тут лучше сделать PositiveSmallIntegerField
-    Так как мы делаем поиск по году, чтобы его ускорить можно добавить индекс.
-    https://ru.stackoverflow.com/questions/976248/django-db-index-in-field-%D1%87%D1%82%D0%BE-%D1%8D%D1%82%D0%BE-%D0%B8-%D0%B7%D0%B0%D1%87%D0%B5%D0%BC"""
     category = models.ForeignKey(
         Category, on_delete=models.SET_NULL,
         null=True,
         related_name='titles',
-        verbose_name='Категория произведения'
+        verbose_name='Категория'
     )
-    rating = models.IntegerField(default=0,
-                                 verbose_name='Рейтинг произведения')  # Тут лучше сделать PositiveSmallIntegerField
+    rating = models.PositiveSmallIntegerField(default=0,
+                                              verbose_name='Рейтинг')
 
     class Meta:
         ordering = ['id']
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
 
     def __str__(self):
         return self.name
@@ -72,11 +68,27 @@ class Title(models.Model):
 class GenreTitle(models.Model):
     """Связная таблица жанры-произведения."""
 
-    genre = models.ForeignKey(Genre, on_delete=models.SET_NULL, null=True)  # Если добавить verbose_name  для каждого поля модели, то в админке будет удобнее управлять моделями. Они должны быть на русском языке.
-    title = models.ForeignKey(Title, on_delete=models.SET_NULL, null=True)  # Если добавить verbose_name  для каждого поля модели, то в админке будет удобнее управлять моделями. Они должны быть на русском языке.
+    genre = models.ForeignKey(Genre, on_delete=models.SET_NULL,
+                              null=True, verbose_name='Жанр')
+    title = models.ForeignKey(Title, on_delete=models.SET_NULL,
+                              null=True, verbose_name='Произведение')
+
+    class Meta:
+        ordering = ['id']
+        verbose_name = 'Жанр произведения'
+        verbose_name_plural = 'Жанр произведений'
 
     def __str__(self):
         return f'{self.genre} {self.title}'
+
+
+""" Для всех моделей надо добавить class Meta и в нем добавить сортировку.
+Код стайл джанги такой, сначала идет класс Meta, а затем метод str
+    class Meta:
+        ...
+
+    def __str__(self):
+        ...  """
 
 
 class Review(models.Model):
